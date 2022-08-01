78d13e60bd9ba14b245e0f287045705386b41f6c
Shaheer Shakir <shaheershakir123@gmail.com>
2022-08-01 16:00:00 -0700

    Initial scaffold and README

---
+++ .full_backup/src/Main.java
@@ -0,0 +1,10 @@
+import java.io.*; 
+import java.util.*;
+
+/** Minimal CSV parser + anomaly detector using rolling z-score. */
+public class Main {
+    static class Tx {
+        String id; long ts; double value;
+        Tx(String id, long ts, double value){ this.id=id; this.ts=ts; this.value=value; }
+    }
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# Goldman Sachs Engineering Campus Hackathon 2022
+
+**Platform:** HackerRank  
+**Type:** Competitive problem-solving used for internship recruitment.  
+Projects/submissions are **not public**; leaderboards visible only to participants.
