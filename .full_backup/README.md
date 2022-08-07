f23094222132cceaacc626449defde35f20fbe6d
Shaheer Shakir <shaheershakir123@gmail.com>
2022-08-06 18:00:00 -0700

    Add basic source files

--- .full_backup/README.md
+++ .full_backup/README.md
@@ -2,4 +2,9 @@
 
 **Platform:** HackerRank  
 **Type:** Competitive problem-solving used for internship recruitment.  
-Projects/submissions are **not public**; leaderboards visible only to participants.
+Projects/submissions are **not public**; leaderboards visible only to participants.
+
+This folder includes a representative final-round style task:
+
+**Financial Data Parser & Anomaly Detector (Java)**  
+- Reads CSV trade logs.  
--- src/Main.java
+++ src/Main.java
@@ -7,4 +7,14 @@ public class Main {
         String id; long ts; double value;
         Tx(String id, long ts, double value){ this.id=id; this.ts=ts; this.value=value; }
     }
-
+
+    public static void main(String[] args) throws Exception {
+        if (args.length==0) {
+            System.err.println("Usage: java Main <csv>");
+            return;
+        }
+        List<Tx> list = readCsv(args[0]);
+        double mean = 0, m2 = 0; // Welford online variance
+        int n = 0;
+        double threshold = 3.0; // z-score
+        System.out.println("id,timestamp,value,z,anomaly");
